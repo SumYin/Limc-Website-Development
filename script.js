1569f079b5b679f7f84403441ecbbe58e6e6dea6
SumYin <ryanzhang333@gmail.com>
2023-04-25 00:56:14 +0200

    ADding hvoer effect

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /* -- Timer -- */
+
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
 let launchDate = new Date("Oct 28, 2023 12:00:00").getTime();
 
@@ -81,3 +82,47 @@ document.getElementById('myForm').addEventListener('submit', async (event) => {
     console.error('Failed to send form data to webhook.');
   }
 });
+
+/* --dark mode-- */
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+var themeToggleBtn = document.getElementById('theme-toggle');
+
+themeToggleBtn.addEventListener('click', function() {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+    
+});
+
+/* -- Morph effect -- */
