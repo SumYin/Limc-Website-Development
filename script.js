7a5f96ee82cd24c72f7907b34654b35b84896587
SumYin <ryanzhang333@gmail.com>
2023-05-15 00:43:26 +0200

    Adding card effect

--- script.js
+++ script.js
@@ -126,17 +126,33 @@ themeToggleBtn.addEventListener('click', function() {
 });
 
 /* -- Morph effect -- */
-document.addEventListener('mousemove', function(e){
-  var blurDiv = document.getElementById('blur-div-1');
-  blurDiv.style.left = e.pageX + 'px';
-  blurDiv.style.top = e.pageY + 'px';
+
+const blurDiv1 = document.getElementById('blur-div-1');
+const section1 = document.getElementById('Welcome');
+
+document.addEventListener('mousemove', (event) => {
+  const section1Rect = section1.getBoundingClientRect();
+  const x = event.clientX - section1Rect.left;
+  const y = event.clientY - section1Rect.top;
+  blurDiv1.style.transform = `translate(${x}px, ${y}px)`;
 });
 
-
-
 const blurDiv2 = document.getElementById('blur-div-2');
-document.addEventListener('mousemove', e => {
-  const x = e.clientX - (blurDiv2.offsetWidth / 2);
-  const y = e.clientY - (blurDiv2.offsetHeight / 2);
+const section2 = document.getElementById('myForm');
+
+document.addEventListener('mousemove', (event) => {
+  const section2Rect = section2.getBoundingClientRect();
+  const x = event.clientX - section2Rect.left;
+  const y = event.clientY - section2Rect.top;
   blurDiv2.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+const blurDiv3 = document.getElementById('blur-div-3');
+const section3 = document.getElementById('map');
+
+document.addEventListener('mousemove', (event) => {
+  const section3Rect = section3.getBoundingClientRect();
+  const x = event.clientX - section3Rect.left;
+  const y = event.clientY - section3Rect.top;
+  blurDiv3.style.transform = `translate(${x}px, ${y}px)`;
 });
