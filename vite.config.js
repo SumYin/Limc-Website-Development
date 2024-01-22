bc5d35173da6e75f793c72732be11d00e1708875
SumYin <ryanzhang333@gmail.com>
2024-01-22 23:56:16 +0100

    Adding Vite Config for Multipage

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: ''
+  base: '',
+  build: {
+    rollupOptions : {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        regulations: resolve(__dirname, 'regulations/index.html'),
+        error: resolve(__dirname, '404/index.html'),
+      }
+    }
+  }
 })
